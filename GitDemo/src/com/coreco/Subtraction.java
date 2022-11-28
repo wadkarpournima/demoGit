7361dbfe958de4aefbdc096b470d06bebeaa7195
Pournima W <wadkarpournima@gmail.com>
2022-11-28 17:34:49 +0530

    added subtraction func

--- GitDemo/src/com/coreco/Subtraction.java
+++ GitDemo/src/com/coreco/Subtraction.java
@@ -3,7 +3,11 @@ package com.coreco;
 public class Subtraction {
 
 	public static void main(String[] args) {
+		int a=5,b=5,c;
+		c=a-b;
+		System.out.println("Subraction is "+c);
 		System.out.println("HEllo from Subtraction");
 		System.out.println("HEllo from Subtraction");
+		
 	}
 }
